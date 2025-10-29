977767881081fd25a00a6bd97ca3f8c655d6c51b
thungdemo <ezongthungdemo@gmail.com>
2025-10-29 23:54:16 +0530

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,55 +6,31 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     @vite('resources/sass/admin.scss')
-    <style>
-        body {
-            background-color: #f9fafb;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .login-card {
-            background: #fff;
-            border-radius: 10px;
-            padding: 2rem;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
-            width: 400px;
-            border: 2px solid #dfdfdf;
-        }
-
-        .logo {
-            display: flex;
-            justify-content: center;
-            margin-bottom: 0.9em;
-        }
-    </style>
 </head>
-<body>
+<body class="login-body">
     <div>
         <div class="logo">
-            <div class="text-center">
+            <div class="text-center  text-light">
                 <img src="{{asset('images/logo.png')}}" alt="Logo" style="height: 100px;">
-                <h4 class="mt-3 mb-2 text-dark">Gauhati High Court Kohima Bench</h4>
-                <p class="text-muted mb-2">Department Login</p>
+                <h4 class="mt-3 mb-2">Gauhati High Court Kohima Bench</h4>
+                <p class="mb-2">Department Login</p>
             </div>
         </div>
         <div class="login-card">
-            <form>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
                     <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
                     @error('email')
-                        <div class="text-dangerk">{{ $message }}</div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
-
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
                     <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
                     @error('password')
-                        <div class="text-danger">{{ $message }}</div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
 
@@ -66,7 +42,7 @@
                     </div>
                     <input type="text" name="captcha" id="captcha" class="form-control @error('captcha') is-invalid @enderror" value="{{ old('captcha') }}" placeholder="Enter the code above">
                     @error('captcha')
-                        <div class="text-danger">{{ $message }}</div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
 
@@ -85,6 +61,5 @@
             </form>
         </div>
     </div>
-
 </body>
 </html>
